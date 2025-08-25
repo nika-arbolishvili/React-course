4eca14e3dadb90f138d0ef014629e20b934abc89
nika-arbolishvili <nikaarbolishvili256@gmail.com>
2025-08-25 17:06:53 +0400

    mission complete

---
+++ task 3/src/src/main.js
@@ -0,0 +1,14 @@
+function scrollevent() {
+  document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 10) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+});
+
+}
+scrollevent();
